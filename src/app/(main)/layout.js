9a1c1626368130f4e0e4e8dff10fe3ff4c3223d3
RasyKy <kyrasy547@gmail.com>
2025-10-10 21:19:21 +0700

    Fixed some issues caused by layout.js

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -17,12 +17,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
+    <>
+      <main
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-      </body>
-    </html>
+      </main>
+    </>
   );
 }
