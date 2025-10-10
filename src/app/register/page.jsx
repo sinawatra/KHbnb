490e83657d7e933f903bdb781f50aac4ee3eed17
RasyKy <kyrasy547@gmail.com>
2025-10-10 16:52:07 +0700

    Built the login/signup page

---
+++ src/app/register/page.jsx
@@ -0,0 +1,21 @@
+import AuthForm from "@/components/Auth";
+import Link from "next/link";
+
+export default function RegisterPage() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen bg-background">
+      <div className="w-full min-w-[600px] max-w-md p-8 text-center bg-background">
+        <h1 className="text-primary font-bold text-4xl pb-5">KHbnb</h1>
+        <h3 className="text-black text-2xl font-bold pb-5">Welcome to KHbnb</h3>
+        <p className="pb-5">Sign up to create account</p>
+
+        <div className="min-h-[500px]">
+          <AuthForm />
+          <Link href="/" className="text-m text-muted-foreground mt-6">
+          &lt;&nbsp; Back to home
+        </Link>
+        </div>
+      </div>
+    </main>
+  );
+}
