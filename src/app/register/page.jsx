921b9c621b896bca1ae7b28980cb081f001c15b5
RasyKy <kyrasy547@gmail.com>
2025-10-14 10:02:50 +0700

    Hide scrollbar in /register page

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function RegisterPage() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-background">
-      <div className="w-full min-w-[600px] max-w-md p-8 text-center bg-background">
+    <main className="flex flex-col items-center justify-center min-h-screen bg-background overflow-hidden">
+      <div className="w-full min-w-[600px] max-w-md p-8 text-center bg-background overflow-y-auto hide-scrollbar max-h-screen">
         <h1 className="text-primary font-bold text-4xl pb-5">KHbnb</h1>
         <h3 className="text-black text-2xl font-bold pb-5">Welcome to KHbnb</h3>
         <p className="pb-5">Sign up to create account</p>
