3d3b5a3b13bf49b2dd3d438177db1eae68dfd6de
RasyKy <kyrasy547@gmail.com>
2025-10-17 15:57:57 +0700

    minor ui adjustments to main page

--- src/components/FeaturedProductCard.jsx
+++ src/components/FeaturedProductCard.jsx
@@ -2,11 +2,11 @@ import { MapPin } from "lucide-react";
 
 export default function FeaturedProductCard({ product }) {
   return (
-    <div className="bg-white shadow-md rounded-lg overflow-hidden w-80">
+    <div className="bg-white shadow-md rounded-lg overflow-hidden w-100">
       <img
         src={product.image}
         alt={product.title}
-        className="w-80 h-50 object-cover"
+        className="w-100 h-60 object-cover"
       />
       <div className="p-4">
         <h3 className="text-lg font-semibold">{product.title}</h3>
@@ -15,8 +15,8 @@ export default function FeaturedProductCard({ product }) {
           <p className="text-gray-600 p-2">{product.location}</p>
         </div>
 
-        <div className="flex justify-between items-center mt-6">
-          <span className="text-primary font-bold text-xl">
+        <div className="flex justify-between items-center mt-4 pb-2">
+          <span className="text-primary font-bold">
             ${product.price}{" "}
             <span className="text-gray-400 font-semibold">/ night</span>
           </span>
