05643cf03a432e11fa066804aa13ecf15310c4a8
RasyKy <kyrasy547@gmail.com>
2025-10-17 14:58:56 +0700

    minor changes

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,25 +1,101 @@
 import Image from "next/image";
 import Link from "next/link";
 import Searchbar from "@/components/Seachbar";
+import { Button } from "@/components/ui/button";
+import FeaturedProductCard from "@/components/FeaturedProductCard";
+import { id } from "date-fns/locale/id";
+
+const properties = [
+  {
+    id: "P001",
+    title: "Beachfront Villa",
+    location: "Sihanouk Ville",
+    image: "/temple.jpg",
+    price: 450,
+  },
+  {
+    id: "P002",
+    title: "Cozy Apartment",
+    location: "Phnom Penh",
+    image: "/temple.jpg",
+    price: 120,
+  },
+  {
+    id: "P003",
+    title: "Luxury Resort",
+    location: "Kep",
+    image: "/temple.jpg",
+    price: 230,
+  },
+];
 
 export default function Home() {
   return (
-    <main>
-      <section>
-        <div className="flex">
-          <div>
-            <img src="#" alt="images" className="w-100 h-50 bg-green-400" />
+    <>
+      <main>
+        <section className="p-10">
+          <div className="flex mb-10">
+            <div className="m-5 flex-1">
+              <img
+                src="/temple.jpg"
+                alt="images"
+                className="w-100 h-50 bg-grey-200"
+              />
+            </div>
+            <div className="flex-1 self-center">
+              <div className="w-1/2">
+                <h1 className="font-bold text-[#FFB400] text-3xl">
+                  Find Your Perfect Home Away From Home
+                </h1>
+                <h3 className="font-bold text-[#0000004d] text-lg">
+                  Discover unique properties across Cambodia. From Beachfront,
+                  villas to city apartments
+                </h3>
+              </div>
+            </div>
           </div>
-          <div className="flex flex-col self-center">
-            <h1 className="font-bold text-[#FFB400] text-3xl">Find Your Perfect Home Away From Home</h1>
-            <h3 className="font-bold text-[#0000004d] text-lg">
-              Discover unique properties across Cambodia. From Beachfront,
-              villas to city apartments
-            </h3>
+          <Searchbar />
+        </section>
+
+        <section className="mt-6 p-6">
+          <div className="flex justify-between p-5">
+            <div className="text-left">
+              <h1 className="font-bold text-2xl">Featured Properties</h1>
+              <h2>Handpicked stays for your next adventure</h2>
+            </div>
+            <Button variant="outline" className="mt-4 mb-4 bg-white">
+              <Link href="/properties">View All</Link>
+            </Button>
+          </div>
+          <div className="flex gap-10 p-5">
+            {properties.map((property) => (
+              <FeaturedProductCard key={property.id} product={property} />
+            ))}
+          </div>
+        </section>
+      </main>
+
+      <footer className="mt-10 border-t">
+        <div className="p-10 text-center text-black bg-white flex">
+          <div className="mx-auto text-left">
+            <h1 className="text-primary font-bold text-xl mb-4">KHbnb</h1>
+            <p className="text-gray-500 font-semibold">
+              Your trusted partner for property rentals in Cambodia
+            </p>
+          </div>
+          <div className="mx-auto text-left">
+            <h1 className="font-bold">Province</h1>
+            <p>Siem Reap</p>
+            <p>Kep</p>
+            <p>Kampot</p>
+            <p>Sihanouk Ville</p>
+            <p>Phnom Penh</p>
           </div>
         </div>
-        <Searchbar></Searchbar>
-      </section>
-    </main>
+        <div className="p-10 text-center text-black border-t">
+          &copy; 2025 KHbnb. All rights reserved.
+        </div>
+      </footer>
+    </>
   );
 }
--- src/components/Seachbar.jsx
+++ src/components/Seachbar.jsx
@@ -3,7 +3,14 @@ import { Search } from "lucide-react";
 import { Calendar } from "./ui/calendar";
 import { useState } from "react";
 import { Popover, PopoverTrigger, PopoverContent } from "./ui/popover";
-import { Command, CommandInput, CommandList, CommandGroup, CommandEmpty, CommandItem } from "./ui/command";
+import {
+  Command,
+  CommandInput,
+  CommandList,
+  CommandGroup,
+  CommandEmpty,
+  CommandItem,
+} from "./ui/command";
 import { Button } from "./ui/button";
 
 const provinces = [
@@ -20,9 +27,8 @@ export default function Searchbar() {
   const [checkOut, setCheckOut] = useState();
   const [location, setLocation] = useState("");
 
-
   return (
-    <div className="p-6 bg-white border-b rounded-full">
+    <div className="p-6 bg-white border border-black rounded-full flex">
       <Popover>
         <PopoverTrigger asChild>
           <div className="px-6 py-3 border-r flex flex-col cursor-pointer">
@@ -35,7 +41,7 @@ export default function Searchbar() {
             />
           </div>
         </PopoverTrigger>
-        <PopoverContent className="w-[300px] p-0">
+        <PopoverContent avoidCollisions={false} side="bottom" className="w-[300px] p-0">
           <Command>
             <CommandInput placeholder="Search provinces..." />
             <CommandList>
@@ -65,7 +71,7 @@ export default function Searchbar() {
             <p>Add dates</p>
           </div>
         </PopoverTrigger>
-        <PopoverContent>
+        <PopoverContent avoidCollisions={false} side="bottom" align="start" sideOffset={5}>
           <Calendar mode="single" selected={checkIn} onSelect={setCheckIn} />
         </PopoverContent>
       </Popover>
@@ -78,7 +84,7 @@ export default function Searchbar() {
             <p>Add dates</p>
           </div>
         </PopoverTrigger>
-        <PopoverContent>
+        <PopoverContent avoidCollisions={false} side="bottom" align="start" sideOffset={5}>
           <Calendar mode="single" selected={checkOut} onSelect={setCheckOut} />
         </PopoverContent>
       </Popover>
@@ -91,11 +97,63 @@ export default function Searchbar() {
             <p>0 guests</p>
           </div>
         </PopoverTrigger>
-        <PopoverContent>{/* Custom guest counter */}</PopoverContent>
+        <PopoverContent
+          side="bottom"
+          align="start"
+          sideOffset={5}
+          className="w-[360px] p-0"
+          avoidCollisions={false}
+        >
+          <div className="p-6">
+            <div className="border-b flex justify-between items-center pb-4 mb-4">
+              <div>
+                <p>Adult</p>
+                <p>Ages 13+</p>
+              </div>
+              <div className="flex justify-between items-center">
+                <button className="px-3 py-1 border rounded-full">-</button>
+                <p className="mr-4 ml-4">0</p>
+                <button className="px-3 py-1 border rounded-full">+</button>
+              </div>
+            </div>
+            <div className="border-b flex justify-between items-center pb-4 mb-4">
+              <div>
+                <p>Children</p>
+                <p>Ages 2-12</p>
+              </div>
+              <div className="flex justify-between items-center">
+                <button className="px-3 py-1 border rounded-full">-</button>
+                <p className="mr-4 ml-4">0</p>
+                <button className="px-3 py-1 border rounded-full">+</button>
+              </div>
+            </div>
+            <div className="border-b flex justify-between items-center pb-4 mb-4">
+              <div>
+                <p>Infants</p>
+                <p>Under 2</p>
+              </div>
+              <div className="flex justify-between items-center">
+                <button className="px-3 py-1 border rounded-full">-</button>
+                <p className="mr-4 ml-4">0</p>
+                <button className="px-3 py-1 border rounded-full">+</button>
+              </div>
+            </div>
+            <div className="border-b flex justify-between items-center pb-4 mb-4">
+              <div>
+                <p>Pets</p>
+              </div>
+              <div className="flex justify-between items-center">
+                <button className="px-3 py-1 border rounded-full">-</button>
+                <p className="mr-4 ml-4">0</p>
+                <button className="px-3 py-1 border rounded-full">+</button>
+              </div>
+            </div>
+          </div>
+        </PopoverContent>
       </Popover>
 
       {/* Search Button */}
-      <Button className="rounded-full">
+      <Button className="rounded-full p-3 ml-4 align-center self-center">
         <Search />
       </Button>
     </div>
